ed13fd6a36ab664400c59b2b6b3cee1654f74d62
Babiplima123 <barbara.lima@zup.com.br>
2023-05-03 21:24:24 -0300

    first commit

--- Protocols Demo/main.swift
+++ Protocols Demo/main.swift
@@ -5,7 +5,43 @@
 //  Created by Barbara Ann Pereira Lima on 03/04/23.
 //
 
-import Foundation
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg () {
+        if isFemale {
+        print ("The bird makes a new birds and a shell.")
+    }
+}
+    func fly () {
+    print ("The bird flaps its wings and lifts off into the sky")
+  
+    }
+}
 
-print("Hello, World!")
+class Eagle: Bird {
+    
+    func soar() {
+        print ("The eagle glides in the air using air currents. ")
+    }
+    
+}
+
+class Penguin: Bird {
+    func swin()  {
+        print ("The penguin paddles through the water.")
+    }
+}
+
+let myEagle = Eagle()
+myEagle.layEgg()
+myEagle.fly()
+
+let myPenguin = Penguin()
+myPenguin.layEgg()
+myPenguin.swin()
+myPenguin.fly()
+    
+    
 
